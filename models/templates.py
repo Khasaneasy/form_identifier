12e5ab2659d7f6c6b8944db8138d22b313c662af
Khasaneasy <kabardos.noob@gmail.com>
2024-12-11 14:37:10 +0300

    add_valid

--- models/templates.py
+++ models/templates.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 import re
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Dict, Any
 
 
@@ -27,3 +28,21 @@ class IncomingTemplate(BaseModel):
     def phone_valid(value: str) -> bool:
         regex_phone = r'\+7 \d{3} \d{3} \d{2} \d{2}'
         return bool(re.match(regex_phone, value))
+    
+    @staticmethod
+    def date_valid(value: str) ->bool:
+        for date_format in ['%d.%m.%Y', '%Y-%m-%d']:
+            try:
+                datetime.strptime(value, date_format)
+            except ValueError:
+                continue
+        return  False
+
+
+    @staticmethod
+    def email_valid(value: str) ->bool:
+        try:
+            EmailStr.validate(value)
+            return True
+        except ValueError:
+            return False
